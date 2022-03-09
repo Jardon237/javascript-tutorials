77896a7bf81abc8dbe4fe5783dc3f18071c87b70
Ngwa Jude <ngwajude161@gmail.com>
2022-03-09 11:26:29 +0100

    addding more knowledge on variables

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //javascript code here
 console.log('hello world');  
 
-let name ="Mosh";
-console.log(name);
+let First_name ="Mosh";
+let last_name ="hemadani"
+console.log(First_name , last_name);
+
+//key word used is "let"
+//all code ends in ";"
+//like every language, varaible names are not separated bt spaces
